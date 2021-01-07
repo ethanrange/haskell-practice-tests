ba48e3de9ded853244feaae08a8e639ffafca178
Ethan <65268454+ethanrange@users.noreply.github.com>
2021-01-07 06:30:51 +0000

    Completed 2009 Paper

--- 2009-BDD/BDD.hs
+++ 2009-BDD/BDD.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Maybe
 
 type Index = Int
 
@@ -18,27 +19,44 @@ type BDD = (NodeId, [BDDNode])
 
 -- Pre: The item is in the given table
 lookUp :: Eq a => a -> [(a, b)] -> b
-lookUp 
-  = undefined
+lookUp = (.) fromJust . lookup
 
 checkSat :: BDD -> Env -> Bool
-checkSat 
-  = undefined
+checkSat (i, ns) e = cs (i, lookUp i ns)
+  where
+    cs :: BDDNode -> Bool
+    cs (0, _)       = False 
+    cs (1, _)       = True
+    cs (_, (ix, l, r))
+      | lookUp ix e = cs (r, lookUp r ns)
+      | otherwise   = cs (l, lookUp l ns)
 
 sat :: BDD -> [[(Index, Bool)]]
-sat 
-  = undefined
+sat (i, ns) = st [] (i, lookUp i ns)
+  where
+    st :: Env -> BDDNode -> [Env]
+    st _ (0, _) = []
+    st e (1, _) = [e]
+    st e (_, (ix, l, r))
+      = st ((ix, True) : e) (r, lookUp r ns) ++ st ((ix, False) : e) (l, lookUp l ns) 
 
 ------------------------------------------------------
 -- PART II
 
 simplify :: BExp -> BExp
-simplify 
-  = undefined
+simplify (Not (Prim b))            = Prim (not b)
+simplify (Or (Prim b1) (Prim b2))  = Prim (b1 || b2)
+simplify (And (Prim b1) (Prim b2)) = Prim (b1 && b2)
+simplify b                         = b
 
 restrict :: BExp -> Index -> Bool -> BExp
-restrict 
-  = undefined
+restrict (Prim b) _ _  = Prim b
+restrict (IdRef n) i b
+  | n == i    = Prim b
+  | otherwise = IdRef n
+restrict (Not e) i b   = simplify (Not (restrict e i b))
+restrict (Or l r) i b  = simplify (Or (restrict l i b) (restrict r i b))
+restrict (And l r) i b = simplify (And (restrict l i b) (restrict r i b))
 
 ------------------------------------------------------
 -- PART III
@@ -48,14 +66,18 @@ restrict
 -- The question suggests the following definition (in terms of buildBDD')
 -- but you are free to implement the function differently if you wish.
 buildBDD :: BExp -> [Index] -> BDD
-buildBDD 
-  = undefined
+buildBDD = buildBDD' 2
 
 -- Potential helper function for buildBDD which you are free
 -- to define/modify/ignore/delete/embed as you see fit.
-buildBDD' :: BExp -> NodeId -> [Index] -> BDD
-buildBDD' 
-  = undefined
+buildBDD' :: NodeId -> BExp -> [Index] -> BDD
+buildBDD' _ (Prim n) []
+  | n         = (1, [])
+  | otherwise = (0, [])
+buildBDD' id ex (i : is) = (id, (id, (i, li, ri)) : ls ++ rs)
+  where 
+    (li, ls) = buildBDD' (2 * id) (restrict ex i False) is
+    (ri, rs) = buildBDD' (2 * id + 1) (restrict ex i True) is
 
 ------------------------------------------------------
 -- PART IV
@@ -63,8 +85,20 @@ buildBDD'
 -- Pre: Each variable index in the BExp appears exactly once
 --      in the Index list; there are no other elements
 buildROBDD :: BExp -> [Index] -> BDD
-buildROBDD 
-  = undefined
+buildROBDD e i = (2, [n | n@(_, (_, li, ri)) <- ns, li /= ri])
+  where
+    (_, ns) = buildROBDD' 2 e i
+
+buildROBDD' :: NodeId -> BExp -> [Index] -> BDD
+buildROBDD' _ (Prim n) []
+  | n         = (1, [])
+  | otherwise = (0, [])
+buildROBDD' id ex (i : is) = (id, (id, (i, lf, rf)) : ls ++ rs)
+  where 
+    (li, ls) = buildROBDD' (2 * id) (restrict ex i False) is
+    (ri, rs) = buildROBDD' (2 * id + 1) (restrict ex i True) is
+    lf = last $ li : [lli | (u, (_, lli, lri)) <- ls, lli == lri]
+    rf = last $ ri : [rli | (u, (_, rli, rri)) <- rs, rli == rri]
 
 ------------------------------------------------------
 -- Examples for testing...
